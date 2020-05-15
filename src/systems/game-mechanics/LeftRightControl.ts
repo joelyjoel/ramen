c0a7832dc1f414c3530df8108e2249aa5e6ce703
Joel Plowright <joel@Joels-MacBook-Air.local>
2020-05-15 10:30:47 +0100

    LeftRightControlSystem

--- src/systems/game-mechanics/LeftRightControl.ts
+++ src/systems/game-mechanics/LeftRightControl.ts
@@ -13,7 +13,6 @@ export const LeftRightControlSystem = new System({
     individualBehaviour(e:{velocity: VelocityComponentState; leftRightControl: LeftRightControlComponentState}, io:IOObject) {
         if(io.userInput.downKeys[37]) {
             let xspeed = e.velocity.xspeed - (e.leftRightControl.acceleration || 5) * io.elapsed
-            console.log(xspeed, e.leftRightControl.acceleration)
             return {velocity: {xspeed}}
         } else if(io.userInput.downKeys[39]) {
             return {
